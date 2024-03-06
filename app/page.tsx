2c3492f2f202373cadc639edf63be63b704e81df
Staszek <stasiu94pro@icload.com>
2024-03-06 19:50:42 +0100

    feat: added simply navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Navigation from './layout/navigation/Navigation';
+
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
-        git init
-      </div>
-    </main>
+    <>
+      <Navigation />
+      <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <div className='z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex'>
+          git init
+        </div>
+      </main>
+    </>
   );
 }
